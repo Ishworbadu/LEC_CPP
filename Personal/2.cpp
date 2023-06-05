f1e8d1a5ae9277174371b23b24d3c37cc80ff82a
Ishworbadu <ishworbadu7@gmail.com>
2023-06-05 17:43:54 +0545

    commit

---
+++ Personal/2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class hi
+{
+    int age;
+    string name;
+
+    public:
+    void nice()
+    {
+    cout<<"hello";
+    cin>>age;
+    }
+
+
+};
+
+int main()
+{
+    hi a;
+    a.nice();
+
+    return 0;
+
+
+}
