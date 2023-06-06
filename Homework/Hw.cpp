27ed8cfc04840739bb79ffccf72e3740b6c5a229
Ishwor badu <93984979+Ishworbadu@users.noreply.github.com>
2023-06-06 12:29:41 +0545

    Hw.cpp

--- Homework/Hw.cpp
+++ Homework/Hw.cpp
@@ -1,1 +1,45 @@
 
+#include <iostream>
+using namespace std;
+
+class MyClass {
+private:
+    int value;
+
+public:
+    MyClass() {
+        value = 0;  // Default constructor
+    }
+
+    MyClass(int num) {
+        value = num;  // Parameterized constructor
+    }
+
+    void setValue(int num) {
+        value = num;
+    }
+
+    int getValue() {
+        return value;
+    }
+};
+
+void printValue(MyClass obj) {
+    cout << "Value: " << obj.getValue() << endl;
+}
+
+int main() {
+    MyClass obj1;  // Object using default constructor
+    cout << "Default Constructor: ";
+    printValue(obj1);
+
+    MyClass obj2(5);  // Object using parameterized constructor
+    cout << "Parameterized Constructor: ";
+    printValue(obj2);
+
+    obj1.setValue(10);  // Using object method
+    cout << "Updated Value: ";
+    printValue(obj1);
+
+    return 0;
+}
